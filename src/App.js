ecae07182c90574f86bc764ef0a9f60208489ce4
sabbir hossain <mdsabbir477470@gmail.com>
2022-09-20 15:27:32 +0600

    reactjs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter></Counter>
+      <ExternalUsers></ExternalUsers>
     </div>
   );
 }
+
+function ExternalUsers() {
+  const [users, setUser] = useState([])
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUser(data))
+  }, [])
+  return (
+    <div>
+      <h2>External User</h2>
+      {
+        users.map(user => <User name={user.name} email={user.email} website={user.website} username={ user.username}></User>)
+      }
+    </div>
+  )
+}
+
+function User(props) {
+  return (
+    <div className='user-container'>
+      <h3>Name: {props.name}</h3>
+      <h6>Email: {props.email}</h6>
+      <h4>website: {props.website}</h4>
+      <h4>username: { props.username}</h4>
+    </div>
+  )
+}
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  const increaseCount = () => setCount(count + 1);
+  const decreaseCount = () => setCount(count - 1);
+
+
+    // const increaseCount = () => {
+    //   const newCount = count + 1;
+    //   setCount(newCount);
+  return (
+    <div className='counter'>
+      <h2>Counter {count}</h2>
+      <div className='btn-count'>
+        <button className='btn' onClick={increaseCount}>Increase</button>
+        <button className='btn' onClick={decreaseCount}>Decrease</button>
+      </div>
+    </div>
+  )
+}
+
+/*
+// product dynamic creact
+  const products = [
+    { name: 'laptop', price: 53000 },
+    { name: 'phone', price: 80000 },
+    { name: 'watch', price: 20000 },
+    { name: 'tablet', price: 5000 },
+  ]
+  {/* normal dynamic
+      <Product name="laptop" price="53000"></Product>
+      <Product name="phone" price="90000"></Product>
+      <Product name="watch" price="530"></Product>
+      <Product name="teblat" price="500"></Product> 
+ */
+// {/* product dynamic set */}
+// {
+//   products.map(product => <Product name={product.name} price={product.price}></Product>)
+// }
+
+// function Product(props) {
+//   // props & JSX declare
+//   return (
+//     <div className="product">
+//       <h3>Name: { props.name}</h3>
+//       <p>Price: { props.price}</p>
+//     </div>
+//   )
+// }
 
 export default App;
